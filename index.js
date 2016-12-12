129e8e6fa206e8a74ead197b8700a13b57050f2b
Raj <rajeev.v.singh@gmail.com>
2016-12-12 01:34:38 -0500

    Add colors and icon to console output

--- index.js
+++ index.js
@@ -15,12 +15,12 @@ function prettyFormatTime(timestamp) {
   return new Date(timestamp * 1000).toLocaleTimeString('en-US', {hour: 'numeric', minute: '2-digit'})
 }
 
-function prettyFormat(weatherData) {
-  return `\n   Weather in ${weatherData.name}: \n
-     Condition: ${weatherData.weather[0].description}
-     Temperature: ${convertKtoF(weatherData.main.temp)}°F (High: ${convertKtoF(weatherData.main.temp_max)} / Low: ${convertKtoF(weatherData.main.temp_min)})
-     Sunrise: ${prettyFormatTime(weatherData.sys.sunrise)}
-     Sunset: ${prettyFormatTime(weatherData.sys.sunset)} \n`
+function prettyFormat(weatherData, icon, color) {
+  return `\n ${chalk.cyan.underline.bold(`Weather in ${weatherData.name}`)} \n
+     ${chalk.green.bold('Condition')}: ${icon}  ${chalk[color](weatherData.weather[0].description)}
+     ${chalk.red.bold('Temperature')}: ${convertKtoF(weatherData.main.temp)}°F (High: ${convertKtoF(weatherData.main.temp_max)} / Low: ${convertKtoF(weatherData.main.temp_min)})
+     ${chalk.yellow.bold('Sunrise')}: ${prettyFormatTime(weatherData.sys.sunrise)}
+     ${chalk.gray.bold('Sunset')}: ${prettyFormatTime(weatherData.sys.sunset)} \n`
 }
 
 function getForecast(zipOrCity, country) {
@@ -38,31 +38,33 @@ function getForecast(zipOrCity, country) {
     return res.json()
   }).then( data => {
     let icon
+    let color = config.defaultColor
+
     const condition = data.weather[0].id
     switch(+Math.floor(condition/100)) {
-      case 2: icon = '⛈'; break
-      case 3: icon = '💧'; break
-      case 5: icon = '🌧'; break
+      case 2: icon = '⛈'; color = 'blue'; break
+      case 3: icon = '💧'; color = 'blue'; break
+      case 5: icon = '🌧'; color = 'blue'; break
       case 6: icon = '⛄️'; break
-      case 7: icon = '⬜️'; break
+      case 7: icon = '⬜️'; color = 'gray'; break
       case 8:
         if (Date.now() > data.sys.sunset) {
           icon = '🌘'; break
         }
         switch (condition) {
-          case 800: icon = '☀️'; break
-          case 801: icon = '🌤'; break
-          case 802: icon = '⛅️'; break
+          case 800: icon = '☀️'; color = 'yellow'; break
+          case 801: icon = '🌤'; color = 'yellow'; break
+          case 802: icon = '⛅️'; color = 'yellow'; break
           case 803: icon = '🌥'; break
           case 804: icon = '☁️'; break
         }; break
       case 9:
         switch (condition) {
-          case 900: icon = '🌪'; break
+          case 900: icon = '🌪'; color = 'red';break
           case 901: case 902: case 960: case 961: case 962:
-            icon = '🌊'; break
+            icon = '🌊'; color = 'red'; break
           case 903: icon = '🍦'; break
-          case 904: icon = '🔥'; break
+          case 904: icon = '🔥'; color = 'red'; break
           case 905: case 951: case 952: case 953: case 954:
           case 955: case 956: case 957: case 958: case 959:
             icon = '🍃'; break
@@ -70,7 +72,7 @@ function getForecast(zipOrCity, country) {
         } break
     }
     cmd.run(`echo 'export WEATHER_ICON="${icon}"' > ~/.weather_prompt`)
-    console.log( prettyFormat(data) )
+    console.log( prettyFormat(data, icon, color) )
   })
 }
 
